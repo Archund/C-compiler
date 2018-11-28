408c0221d9a941456f5539c69b5e0bb923eafcd6
Archund <jarchund@gmail.com>
2018-11-27 20:05:58 -0600

    Organized files, added testing script
    Fixed emitter for '*', '(', and ')'
    Changes to be committed:
    	new file:   RUN
    	modified:   bin/exec
    	modified:   compile
    	new file:   pruebas
    	renamed:    emitter.c -> src/emitter.c
    	renamed:    error.c -> src/error.c
    	renamed:    global.h -> src/global.h
    	renamed:    lexer.c -> src/lexer.c
    	renamed:    main.c -> src/main.c
    	renamed:    sintax.c -> src/sintax.c
    	renamed:    start.c -> src/startc
    	renamed:    symbols.c -> src/symbols.c

--- src/emitter.c
+++ src/emitter.c
@@ -2,21 +2,31 @@
 
 void emitter(int t, int tval) { /* genera  la salida  */
   switch(t) {
-    case '+': case '-': case '/': 
+
+    case '+': case '-': case '/': case '*':
       printf("%c\n", t);  
     break;
+
     case DIV:
       printf("DIV\n");  
     break;
+
+    case '(': case ')':
+      ;//print nothing
+    break;
+
     case MOD:
       printf("MOD\n");
       break;
+
     case NUM:
       printf("%d\n", tval); 
     break;
+
     case ID:
       printf("%s\n", symbolTable[tval].aplex);
     break;
+
     default:
       printf("complex %d,  tokenVal %d\n", t, tval);
   }
